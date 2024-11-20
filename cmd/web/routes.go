64713ab1b8e151e4623477deef30575d8208bde1
Teguh Samudra <teguhsam9@gmail.com>
2024-11-19 18:14:23 -0800

    produce receipt

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-) 
+)
 
-func (app *application) routes() http.Handler{
+func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 
 	return mux
 }
-
