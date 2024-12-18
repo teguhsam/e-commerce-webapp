a48770776f4b92527ada8d6a6e5f82f24e2bdf3d
Teguh Samudra <teguhsam9@gmail.com>
2024-12-17 16:55:21 -0800

    update database function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"ecomm/internal/models"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
@@ -45,12 +47,13 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 // ChargeOnce displays the page to buy one widget
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 
-	widget := models.Widget{
-		ID:             1,
-		Name:           "Custom Widget",
-		Description:    "A very nice widget",
-		InventoryLevel: 10,
-		Price:          1000,
+	id := chi.URLParam(r, "id")
+	widgetID, _ := strconv.Atoi(id)
+
+	widget, err := app.DB.GetWidget(widgetID)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
 	}
 
 	data := make(map[string]interface{})
