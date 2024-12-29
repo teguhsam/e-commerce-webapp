6541ce189d294297ea3d87be2baa549ff69987be
Teguh Samudra <teguhsam9@gmail.com>
2024-12-29 10:25:34 -0800

    Revising our Virtual Terminal

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,11 +12,16 @@ func (app *application) routes() http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/", app.Home)
+
+	// Virtual Terminal
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
+
+	// Others
+	mux.Get("/widget/{id}", app.ChargeOnce)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 	mux.Get("/receipt", app.Receipt)
-
-	mux.Get("/widget/{id}", app.ChargeOnce)
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
