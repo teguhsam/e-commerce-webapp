1c86ece9780e3d1fd33bbd7f91c2cc40598dc3ba
Teguh Samudra <teguhsam9@gmail.com>
2025-01-07 12:48:34 -0800

    create a writeJSON helper function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -45,3 +45,23 @@ func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err e
 	w.Write(out)
 	return nil
 }
+
+// writeJSON writes arbitrary data out as JSON
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+
+	return nil
+}
