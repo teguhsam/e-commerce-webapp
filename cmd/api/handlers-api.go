98fd06115eaf50b2abfaf7365f4385e622d910d6
Teguh Samudra <teguhsam9@gmail.com>
2025-01-19 09:14:55 -0800

    Creating our mail templates and sending a test email

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -422,5 +422,17 @@ func (app *application) SendPasswordResetEmail(w http.ResponseWriter, r *http.Re
 	data.Link = "http://www.test.ca"
 
 	// send mail
+	err = app.SendMail("info@widgets.com", "info@widgets.com", "Password Reset Request", "password-reset", data)
+	if err != nil {
+		app.errorLog.Println(err)
+		app.badRequest(w, r, err)
+		return
+	}
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
 
+	resp.Error = false
+	app.writeJSON(w, http.StatusCreated, resp)
 }
